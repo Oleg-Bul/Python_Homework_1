a4f61a2c903e71e2201459515e75afd40526c9a0
Oleg_bul <kokseg63@gmail.com>
2022-11-23 13:57:44 +0400

    task 2 complete

--- Home_work_1_2.py
+++ Home_work_1_2.py
@@ -3,3 +3,13 @@
 # ⋁ - "Или"
 # ⋀ - "И"
 # ¬ - "Не"
+
+print('Введите X,Y,Z (True или False, 1 или 0) для  утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z ')
+x = bool(input('X: '))
+y = bool(input('Y: '))
+z = bool(input('Z: '))
+
+if not(x or y or z) == ((not x) and (not y) and (not z)):
+    print ('Утверждение ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z верно')
+else:
+    print()
